6f616722b9539bb5702b74dbc6cef7e98faf424d
George Christidis <ggg.xristidis@yahoo.gr>
2025-05-22 07:11:16 +0300

    Created registration succeed page

---
+++ javascript/registration_succeed.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(() => {
+        window.location.assign("../php/main_page.php");
+    }, 3000);
+});
---
+++ php/customer_register_succeed.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Registration Success</title>
+    <link rel="stylesheet" href="../css/registration_succeed.css" />
+    <script src="../javascript/registration_succeed.js"></script>
+</head>
+<body>
+    <main>
+        <h1>Registration was sucessful!</h1>
+    </main>
+</body>
+</html>
