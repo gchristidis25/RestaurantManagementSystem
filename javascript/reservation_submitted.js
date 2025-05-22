e0ec344c1faac22ad819a2593800c60f9d12decc
George Christidis <ggg.xristidis@yahoo.gr>
2025-05-22 07:11:54 +0300

    Created reservation submitted page

---
+++ javascript/reservation_submitted.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(() => {
+        window.location.assign("../php/customer_dashboard.php");
+    }, 3000);
+});
--- php/reservation_submitted.php
+++ php/reservation_submitted.php
@@ -1,1 +1,16 @@
-<h2>RESERVATION SUBMITTED</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Reservation Submitted</title>
+    <link rel="stylesheet" href="../css/reservation_submitted.css" />
+    <script src="../javascript/reservation_submitted.js"></script>
+</head>
+<body>
+    <main>
+        <h1>Reservation Submitted!</h1>
+        <p>Thank you for your reservation. We have received your request and will process it shortly.</p>
+    </main>
+</body>
+</html>
