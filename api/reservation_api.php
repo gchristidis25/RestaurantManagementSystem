4a47d485ab7590ced643d33466d933b360c5206d
George Christidis <ggg.xristidis@yahoo.gr>
2025-05-22 07:16:06 +0300

    Created the reservation management API

--- api/reservation_api.php
+++ api/reservation_api.php
@@ -53,7 +53,7 @@ function get_capacity_in_timeslot($db, $date, $start_time)
         return $row["Capacity"] ?? 0;
 }
 
-function get_reservations($db, $customer_id, $status)
+function get_reservations_by_customer($db, $customer_id, $status)
 {
     $stmt = $db->prepare("SELECT * FROM Reservation WHERE AccountID = ? AND Status = ? ORDER BY Reservation_date ASC");
     $stmt->bind_param("is", $customer_id, $status);
@@ -68,37 +68,80 @@ function get_reservations($db, $customer_id, $status)
     return $confirmed_reservations;
 }
 
+function get_waiter_reservations($db, $date, $timeslot, $status)
+{
+    $stmt = $db->prepare("SELECT Reservation.ReservationID, Customer.Customer_name, Customer.Customer_surname, NumberOfGuests, Reservation_notes
+    FROM Reservation
+    JOIN Customer ON Reservation.AccountID = Customer.AccountID
+    WHERE Reservation_date = ? AND Start_time = ? AND Status = ?");
+    $stmt->bind_param("sss", $date, $timeslot, $status);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $reservations = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $reservations[] = $row;
+    }
+
+    return $reservations;
+}
+
+function update_status($db, $reservation_id, $new_status) {
+    $stmt = $db->prepare("UPDATE Reservation SET Status = ? WHERE ReservationID = ?");
+    $stmt->bind_param("si", $new_status, $reservation_id);
+    $stmt->execute();
+}
+
 switch ($method)
 {
     case "POST":
         $rawData = file_get_contents("php://input");
         $data = json_decode($rawData, true);
 
-        $customer_id = $data["customerID"];
-        $number_of_guests = $data["numberGuests"];
-        $date = $data["date"];
-        $start_time = $data["startTime"];
-        $end_time = $data["endTime"];
-        $reservation_details = $data["reservationDetails"];
+        if (isset($data["method"]) && $data["method"] === "PATCH") // Updating the status od a reservation
+        {
+        $reservation_id = $data["id"];
+        $new_status = $data["status"];
+        update_status($restaurant_db, $reservation_id, $new_status);
+        echo json_encode(["message" => "Reservation status updated."]);
+        }
+        else // Creating a new reservation
+        {
+            $customer_id = $data["customerID"];
+            $number_of_guests = $data["numberGuests"];
+            $date = $data["date"];
+            $start_time = $data["startTime"];
+            $end_time = $data["endTime"];
+            $reservation_details = $data["reservationDetails"];
 
-        create_new_reservation(
-            $restaurant_db, $customer_id, $number_of_guests, $date,
-            $start_time, $end_time, $reservation_details
-        );
+            create_new_reservation(
+                $restaurant_db, $customer_id, $number_of_guests, $date,
+                $start_time, $end_time, $reservation_details
+            );
+        }
         break;
-    case "GET":
+    case "GET": 
         $action = $_GET['action'];
-        if ($action === "MAX_CAPACITY")
+        if ($action === "MAX_CAPACITY") // get the max capacity of the floor in a specific timeslot
         {
             $guest_max_capacity = get_guest_max_capacity();
             echo json_encode(["max_capacity" => $guest_max_capacity]);
         }
-        else if ($action === "RESERVATIONS")
+        else if ($action === "CUSTOMER_RESERVATIONS") // Get the reservations of a customer
         {
             $customer_id = $_GET["customerID"];
-            $pending_reservations = get_reservations($restaurant_db, $customer_id, "Pending");
-            $confirmed_reservations = get_reservations($restaurant_db, $customer_id, "Confirmed");
+            $pending_reservations = get_reservations_by_customer($restaurant_db, $customer_id, "Pending");
+            $confirmed_reservations = get_reservations_by_customer($restaurant_db, $customer_id, "Confirmed");
             echo json_encode(["pending" => $pending_reservations, "confirmed" => $confirmed_reservations]);
+        }
+        else if ($action === "WAITER_RESERVATIONS") // Get the reservations of a specific day, timeslot, status for the Reservation Management System
+        {
+            $date = $_GET["date"];
+            $timeslot = $_GET["timeslot"];
+            $status = $_GET["status"];
+
+            $reservations = get_waiter_reservations($restaurant_db, $date, $timeslot, $status);
+            echo json_encode(["reservations" => $reservations]);
         }
         else 
         {
@@ -117,6 +160,6 @@ switch ($method)
         break;
     default:
         http_response_code(405);
-        echo json_encode(["error" => "Method not allowed"]);
+        echo json_encode(["error" => "gdfg not allowed"]);
         break;
 }
