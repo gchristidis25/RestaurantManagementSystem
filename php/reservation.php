96e43cd044af217ac2e9af4a209db6abb563da2b
George Christidis <ggg.xristidis@yahoo.gr>
2025-05-22 07:12:27 +0300

    Created reservation page

--- php/reservation.php
+++ php/reservation.php
@@ -16,23 +16,24 @@ $id = $_SESSION["id"];
     <head>
         <meta charset="UTF-8">
         <title>Reservation Page</title>
-        <!-- <link rel="stylesheet" href="../css/reservation.css"> -->
+        <link rel="stylesheet" href="../css/reservation.css">
         <script src="../javascript/reservation.js"></script>
     </head>
     <body>
         <main>
             <div class="container" id="<?php echo $id ?>">
-            <form id="reservation-form">
-                <div class="form-item">
-                    <label for="date">Reservation Date</label>
-                    <input type="date" id="date" name="date" required>
-                </div>
-                <div class="form-item">
-                    <label for="num-guests">Number of Guests</label>
-                    <input type="number" id="num-guests" name="num-guests" min="0" max="9" required>
-                </div>
-                <button id="submit">Search for available timeslots</button>
-            </form>
+                <form id="reservation-form">
+                    <div class="form-item">
+                        <label for="date">Reservation Date</label>
+                        <input type="date" id="date" name="date" required>
+                    </div>
+                    <div class="form-item">
+                        <label for="num-guests">Number of Guests</label>
+                        <input type="number" id="num-guests" name="num-guests" min="0" max="9" required>
+                    </div>
+                    <button id="submit">Search for available timeslots</button>
+                </form>
+                <button id="return-button"><img src="../images/return_icon.svg"></button>
             </div>
         </main>
     </body>
